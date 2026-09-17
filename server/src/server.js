ff5d7542577688a9703e8d03dccd53a258f99b88
srinivastg23 <saisrinivastg@gmail.com>
2026-09-17 12:05:05 +0530

    Fix catch-all route for frontend serving

--- server/src/server.js
+++ server/src/server.js
@@ -66,7 +66,7 @@ app.use('/api/projects', projectRoutes)
 
 app.use(express.static(path.join(__dirname, '../../client/dist')))
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'))
 })
 
